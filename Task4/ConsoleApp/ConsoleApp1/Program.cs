121baa56affa7e6ca79e69c4d0dd0d82904777b5
iksusa <rudkoksenia80@gmail.com>
2025-12-25 20:39:24 +0300

    Add files via upload

---
+++ Task4/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Выберите размер вклада:");
+        Console.WriteLine("1 - меньше 100");
+        Console.WriteLine("2 - от 100 до 200");
+        Console.WriteLine("3 - больше 200");
+
+        string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            Console.WriteLine("Начислено 5%");
+        }
+        else if (choice == "2")
+        {
+            Console.WriteLine("Начислено 7%");
+        }
+        else if (choice == "3")
+        {
+            Console.WriteLine("Начислено 10%");
+        }
+        else
+        {
+            Console.WriteLine("Неверный ввод");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ task2/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        string number = Console.ReadLine();
+
+        if (number == "6" || number == "7" || number == "8" || number == "9")
+        {
+            Console.WriteLine("Число больше 5 и меньше 10");
+        }
+        else
+        {
+            Console.WriteLine("Неизвестное число");
+        }
+
+        Console.ReadLine();
+    }
+}
