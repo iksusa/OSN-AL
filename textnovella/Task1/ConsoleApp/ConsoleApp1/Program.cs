3462af2fa7c2295acde985018fcee5a11db34532
iksusa <rudkoksenia80@gmail.com>
2025-12-26 11:18:00 +0300

    дз

---
+++ textnovella/Task1/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        string a = Console.ReadLine();
+
+        Console.WriteLine("Введите второе число:");
+        string b = Console.ReadLine();
+
+        if (a == b)
+        {
+            Console.WriteLine("Числа равны");
+        }
+        else
+        {
+            Console.WriteLine("Числа не равны");
+        }
+
+        Console.ReadLine();
+    }
+}
