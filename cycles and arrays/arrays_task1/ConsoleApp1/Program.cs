fb3d3c2609263b3312d060a7b17a3e57580df3a0
iksusa <rudkoksenia80@gmail.com>
2025-12-26 11:07:10 +0300

    Add files via upload

---
+++ cycles and arrays/arrays_task1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a >= 0 && a <= 10 && b >= 0 && b <= 10)
+            {
+                Console.WriteLine("Результат: " + (a * b));
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Числа введены неверно, попробуйте ещё раз.");
+            }
+        }
+    }
+}
